244b9f89641578096448e62343f9cf187eac6c95
Fadut <fato0049@edu.zealand.dk>
2023-12-08 20:27:32 +0100

    Added flask and functionality in lokaler
    Flask file has been added, and can now be used to get http requests

--- Pages/Lokale/main.js
+++ Pages/Lokale/main.js
@@ -20,7 +20,7 @@ const app = Vue.createApp({
         async addLokale() {
             try {
                 // Make a POST request to your backend API to add a new lokale
-                const response = await axios.post('YOUR_BACKEND_API_ENDPOINT', {
+                const response = await axios.post('http://127.0.0.1:5000/lokaler', {
                     name: this.newLokale,
                 });
 
