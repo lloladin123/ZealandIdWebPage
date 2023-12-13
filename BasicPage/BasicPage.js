cbfea15bfbae7e3abed53d885b079acf00be7ca4
SamiArlimmi <samiarlimmi1@gmail.com>
2023-12-13 09:46:37 +0100

    Revert "hej"
    This reverts commit d4ba16d6ad53489988f5b68155029ef8b36df992, reversing
    changes made to e50226e5abf74cd55d385ddaf8b441cec38913dd.

---
+++ BasicPage/BasicPage.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            list: [],
+            string: "",
+            int: 1,
+            bool: true,
+        }
+    },
+    methods: {
+        method1()
+        {
+
+        }
+    }
+}).mount("#app")
